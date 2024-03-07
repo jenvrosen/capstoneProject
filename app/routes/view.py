11f93918d63f8273abd8c8d20e9a3e3d667d2995
scando08 <sof.cando@gmail.com>
2024-03-06 16:06:47 -0800

    Fixed bug in navbar

--- app/routes/view.py
+++ app/routes/view.py
@@ -6,24 +6,24 @@ view_blueprint = Blueprint('view', __name__)
 #Render log in page
 @view_blueprint.route('/')
 def login():
-    return render_template('login.html', hideNavigation=False)
+    return render_template('login.html', hideNavigation=True)
 
 # Render the Administrator page
 @view_blueprint.route('/admin')
 def admin():
-    return render_template('admin.html')
+    return render_template('admin.html', hideNavigation=False)
 
 # Render the Profile page
 @view_blueprint.route('/myprofile')
 def my_profile():
-    return render_template('myprofile.html') 
+    return render_template('myprofile.html', hideNavigation=False) 
 
-# Render the Administrator page
+# Render the Home page
 @view_blueprint.route('/home')
 def home():
-    return render_template('home.html') 
+    return render_template('home.html', hideNavigation=False) 
 
 @view_blueprint.route('/signout')
 def sign_out():
     # Add sign out logic here
-    return redirect(url_for('main.login')) 
+    return redirect(url_for('view.login')) 
